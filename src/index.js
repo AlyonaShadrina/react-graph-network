f4ed50ce51348b4e5832d8d3114ee5764a21a719
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-16 09:59:04 +0200

    [fix] to check if problem is in npm link (temp)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { select } from "d3-selection";
 import { forceCenter, forceLink, forceManyBody, forceSimulation } from "d3-force";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { tick } from './events';
 import { addDrag, addHoverOpacity, addZoom } from './interactions';
@@ -10,19 +10,20 @@ import { addDrag, addHoverOpacity, addZoom } from './interactions';
 // TODO: add centering node on click
 // TODO: fix function with hook
 
-// const Graph = ({
-//     data,
-//     nodeDistance,
-//     NodeComponent,
-//     LineComponent,
-//     pullIn,
-//     zoomDepth,
-//     enableDrag,
-//     hoverOpacity,
-//     ...restProps
-// }) => {
+const Graph = ({
+    data,
+    nodeDistance,
+    NodeComponent,
+    LineComponent,
+    pullIn,
+    zoomDepth,
+    enableDrag,
+    hoverOpacity,
+    ...restProps
+}) => {
+    // const [count, setCount] = useState(0);
 //
-//     useEffect(() => {
+    useEffect(() => {
 //
 //         const svg = select("#GraphTree_container");
 //         const link = svg.selectAll("._graphLine").data(data.links);
@@ -45,40 +46,152 @@ import { addDrag, addHoverOpacity, addZoom } from './interactions';
 //         addHoverOpacity(node, link, hoverOpacity);
 //         addDrag(node, simulation, enableDrag, pullIn);
 //
-//     }, [data, nodeDistance, NodeComponent, LineComponent, pullIn, zoomDepth, enableDrag, hoverOpacity,]);
+        console.log('bla');
+    }, []);
+    // }, [data, nodeDistance, NodeComponent, LineComponent, pullIn, zoomDepth, enableDrag, hoverOpacity,]);
 //
 //
-//     return (
-//         <svg
-//             id="GraphTree_container"
-//             width="100%"
-//             height="100%"
-//             {...restProps}
-//         >
-//             <g className="_graphZoom">
-//                 {
-//                     data.links.map((link, i) => {
-//                         return LineComponent
-//                             ? <LineComponent link={link} key={i} className="_graphLine"/>
-//                             : <line stroke="grey" key={i} className="_graphLine" />
-//                     })
-//                 }
-//                 {
-//                     data.nodes.map((node, i) => {
-//                         return (
-//                             <g key={i} className="_graphNode">
-//                                 {
-//                                     NodeComponent
-//                                         ? <NodeComponent node={node}/>
-//                                         : <circle fill="black" r={10} />
-//                                 }
-//                             </g>
-//                         )
-//                     })
-//                 }
-//             </g>
-//         </svg>
-//     )
+    return (
+        <svg
+            id="GraphTree_container"
+            width="100%"
+            height="100%"
+            {...restProps}
+        >
+{/*//             <g className="_graphZoom">*/}
+{/*//                 {*/}
+{/*//                     data.links.map((link, i) => {*/}
+{/*//                         return LineComponent*/}
+{/*//                             ? <LineComponent link={link} key={i} className="_graphLine"/>*/}
+{/*//                             : <line stroke="grey" key={i} className="_graphLine" />*/}
+{/*//                     })*/}
+{/*//                 }*/}
+{/*//                 {*/}
+{/*//                     data.nodes.map((node, i) => {*/}
+{/*//                         return (*/}
+{/*//                             <g key={i} className="_graphNode">*/}
+{/*//                                 {*/}
+{/*//                                     NodeComponent*/}
+{/*//                                         ? <NodeComponent node={node}/>*/}
+{/*//                                         : <circle fill="black" r={10} />*/}
+{/*//                                 }*/}
+{/*//                             </g>*/}
+{/*//                         )*/}
+{/*//                     })*/}
+{/*//                 }*/}
+{/*//             </g>*/}
+        </svg>
+    )
+};
+
+// Graph.defaultProps = {
+//     nodeDistance: 100,
+//     zoomDepth: 0,
+//     hoverOpacity: 1,
+// };
+
+export default Graph;
+
+
+// class Graph extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = null;
+//     }
+//
+//     renderGraph(props) {
+//         const {
+//             data,
+//             nodeDistance,
+//             pullIn,
+//             zoomDepth,
+//             enableDrag,
+//             hoverOpacity,
+//         } = props;
+//
+//         if (!data) {
+//             return null
+//         }
+//
+//         const svg = select("#GraphTree_container");
+//         const link = svg.selectAll("._graphLine").data(data.links);
+//         const node = svg.selectAll("._graphNode").data(data.nodes);
+//
+//         const simulation = forceSimulation(data.nodes)
+//             .force("link", forceLink()                                 // This force provides links between nodes
+//                 .id(function(d) { return d.id; })                      // This provide the id of a node
+//                 .links(data.links)                                     // and this the list of links
+//             )
+//             .force("charge", forceManyBody().strength(-1 * nodeDistance))          // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+//             .force("center", forceCenter(
+//                 svg._groups[0][0].parentElement.clientWidth / 2,
+//                 svg._groups[0][0].parentElement.clientHeight / 2
+//             ))                                                         // This force attracts nodes to the center of the svg area
+//             .on("tick", () => tick(node, link));                       // https://github.com/d3/d3-force#simulation_tick
+//
+//         // add interactions
+//         addZoom(svg, zoomDepth);
+//         addHoverOpacity(node, link, hoverOpacity);
+//         addDrag(node, simulation, enableDrag, pullIn);
+//     }
+//
+//     componentDidMount() {
+//         this.setState(this.props);
+//         this.renderGraph(this.props);
+//     };
+//
+//     render() {
+//         if (this.state) {
+//             this.renderGraph(this.props)
+//         }
+//         const {
+//             data,
+//             nodeDistance,
+//             NodeComponent,
+//             LineComponent,
+//             pullIn,
+//             zoomDepth,
+//             enableDrag,
+//             hoverOpacity,
+//             ...restProps
+//         } = this.props;
+//
+//         if (!data) {
+//             return null
+//         }
+//
+//         return (
+//             <svg
+//                 id="GraphTree_container"
+//                 width="100%"
+//                 height="100%"
+//                 {...restProps}
+//             >
+//                 <g className="_graphZoom">
+//                     {
+//                         data.links.map((link, i) => {
+//                             return LineComponent
+//                                 ? <LineComponent link={link} key={i} className="_graphLine"/>
+//                                 : <line stroke="grey" key={i} className="_graphLine" />
+//                         })
+//                     }
+//                     {
+//                         data.nodes.map((node, i) => {
+//                             return (
+//                                 <g key={i} className="_graphNode">
+//                                     {
+//                                         NodeComponent
+//                                             ? <NodeComponent node={node}/>
+//                                             : <circle fill="black" r={10} />
+//                                     }
+//                                 </g>
+//                             )
+//                         })
+//                     }
+//                 </g>
+//             </svg>
+//         )
+//     }
 // };
 //
 // Graph.defaultProps = {
@@ -88,113 +201,3 @@ import { addDrag, addHoverOpacity, addZoom } from './interactions';
 // };
 //
 // export default Graph;
-
-
-class Graph extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = null;
-    }
-
-    renderGraph(props) {
-        const {
-            data,
-            nodeDistance,
-            pullIn,
-            zoomDepth,
-            enableDrag,
-            hoverOpacity,
-        } = props;
-
-        if (!data) {
-            return null
-        }
-
-        const svg = select("#GraphTree_container");
-        const link = svg.selectAll("._graphLine").data(data.links);
-        const node = svg.selectAll("._graphNode").data(data.nodes);
-
-        const simulation = forceSimulation(data.nodes)
-            .force("link", forceLink()                                 // This force provides links between nodes
-                .id(function(d) { return d.id; })                      // This provide the id of a node
-                .links(data.links)                                     // and this the list of links
-            )
-            .force("charge", forceManyBody().strength(-1 * nodeDistance))          // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-            .force("center", forceCenter(
-                svg._groups[0][0].parentElement.clientWidth / 2,
-                svg._groups[0][0].parentElement.clientHeight / 2
-            ))                                                         // This force attracts nodes to the center of the svg area
-            .on("tick", () => tick(node, link));                       // https://github.com/d3/d3-force#simulation_tick
-
-        // add interactions
-        addZoom(svg, zoomDepth);
-        addHoverOpacity(node, link, hoverOpacity);
-        addDrag(node, simulation, enableDrag, pullIn);
-    }
-
-    componentDidMount() {
-        this.setState(this.props);
-        this.renderGraph(this.props);
-    };
-
-    render() {
-        if (this.state) {
-            this.renderGraph(this.props)
-        }
-        const {
-            data,
-            nodeDistance,
-            NodeComponent,
-            LineComponent,
-            pullIn,
-            zoomDepth,
-            enableDrag,
-            hoverOpacity,
-            ...restProps
-        } = this.props;
-
-        if (!data) {
-            return null
-        }
-
-        return (
-            <svg
-                id="GraphTree_container"
-                width="100%"
-                height="100%"
-                {...restProps}
-            >
-                <g className="_graphZoom">
-                    {
-                        data.links.map((link, i) => {
-                            return LineComponent
-                                ? <LineComponent link={link} key={i} className="_graphLine"/>
-                                : <line stroke="grey" key={i} className="_graphLine" />
-                        })
-                    }
-                    {
-                        data.nodes.map((node, i) => {
-                            return (
-                                <g key={i} className="_graphNode">
-                                    {
-                                        NodeComponent
-                                            ? <NodeComponent node={node}/>
-                                            : <circle fill="black" r={10} />
-                                    }
-                                </g>
-                            )
-                        })
-                    }
-                </g>
-            </svg>
-        )
-    }
-};
-
-Graph.defaultProps = {
-    nodeDistance: 100,
-    zoomDepth: 0,
-    hoverOpacity: 1,
-};
-
-export default Graph;
